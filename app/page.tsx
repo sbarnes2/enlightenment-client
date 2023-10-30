0ec63104fc6439cb7e5193f71285ad3df13e6236
Sean Barnes <sean.barnes@mybinxhealth.com>
2023-10-30 10:15:10 +0000

    update links

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 
 export default function Home() {
   return (
@@ -10,8 +9,7 @@ export default function Home() {
         <div className='linksdiv'>
           <ul>
             <li className='linklistitem'><Link href="users">Users List</Link></li>
-            {/* <li className='linklistitem'><Link href="manager">Managers List</Link></li> */}
-            <li className='linklistitem'><Link href='documents'>Documents List</Link></li>
+            <li className='linklistitem'><Link href="documents">User Documents List</Link></li>
           </ul>
         </div>
         </main>
