fc8c995f964fed1e5004e56a8eb32ed263ccf235
Sean Barnes <sean.barnes@mybinxhealth.com>
2023-11-01 11:16:13 +0000

    interim changes saved to move code

--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -1,43 +1,56 @@
 import React from 'react'
 import Userdocument from '../components/Userdocument'
 
-interface userdocumentitem{
-    username:string
-    documentid:number;
-    documentnumber:string;
-    documentversion:string;
-    training_complete_date:string;
-}
+interface userdocumentstatusitem{
+    userid:number;
+    username:string;
+    email_address:string;
+    documentqtid:string;
+    usercurrentrevision:string;
+    rev:string;
+};
+
 interface u {
     id: number;
     username: string;
     email_address:string;
     firstname:string;
     surname:string;
-    }
+};
+
+function handleClick (){
+    console.log('clicked');
+}
 
 
 const DocumentsPage  = async () => {
     
-    const user_c = await fetch(`http://localhost:8080/api/users/user/21`,);
-    const current_user : u = await user_c.json();
+/*     const user_c = await fetch(`http://localhost:8080/api/users/user/21`,);
+    const current_user : u [] = await user_c.json();
+ */
+    const userid:number = 21;
+    const res = await fetch(`http://localhost:8080/api/documents/getdocumentsbyuserid/${userid}`,);
+    const docs : userdocumentstatusitem [] = await res.json();
 
-    const res = await fetch(`http://localhost:8080/api/documents/getdocumentsbyuserid/21`,);
-    const docs : userdocumentitem [] = await res.json();
-
-   // tslint:disable-next-line:no-console  
-    console.log(user_c.json());
-
+    console.log('number of user documents : '+docs.length);
+    console.log(docs[23]);
     return (
-    <div className='name'><b>{current_user.firstname}'s Documents List</b><div/>
-        <ul>
-            {/* {users.map(user =><li key= {user.id}>{user.username}</li>)} */}
-            {docs.map(doc =>
-            <li key={doc.documentid}>
-                <Userdocument documentid={doc.documentid} documentnumber={doc.documentnumber} training_complete_date={doc.training_complete_date} 
-                documentversion={doc.documentversion}/> 
-            </li>)}
-        </ul>
+    <div>
+        <p>Please Review the following documents :</p>
+        <br></br>
+            <table >
+                <thead><tr><th>Document Number</th><th>Current Document Revision</th><th>Target Document Revision</th></tr></thead>
+                <tbody>
+                {docs.map(doc =>
+                    <tr key={doc.documentqtid}>
+                        <td>{doc.documentqtid}</td>
+                        <td className='item-doc-version'>{doc.usercurrentrevision}</td>
+                        <td className='item-doc-version'>{doc.rev}</td>
+                        <td><button className='btn btn-blue' onClick={handleClick} >Reviewed</button></td>
+                    </tr>
+                )}
+                </tbody>
+            </table>
     </div>
   )
 }  
