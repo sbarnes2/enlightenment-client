1b993af0ea44efc5281b9c07838a545440234c7c
Sean Barnes <sean.barnes@mybinxhealth.com>
2023-11-29 15:28:51 +0000

    minor changes

--- app/components/TeamButtonSet.tsx
+++ app/components/TeamButtonSet.tsx
@@ -4,17 +4,17 @@ import { useRouter } from 'next/navigation';
 
 
 
-function TeamButtonSet (teamid:any):any {
+function TeamButtonSet (params:any):any {
     const router = useRouter();
 
     function handleAddTeam(teamid:any){router.push('/admin/users/add');}
-    function handleManageRoles(){router.push('/admin/roles/team/'+teamid);}
+    function handleManageRoles(teamid:any){router.push('/admin/roles/team/'+teamid);}
     function handleTeamDocuments(){}
 
     return (
         <div className='grid gap-4 grid-cols-3 items-center'>
             <button className='btn btn-accent col-span-1' onClick={()=>handleAddTeam(teamid)}>Add Team Member</button>
-            <button className='btn btn-accent col-span-1' onClick={()=>handleManageRoles}>Manage Team Roles</button>
+            <button className='btn btn-accent col-span-1' onClick={()=>handleManageRoles(params.id)}>Manage Team Roles</button>
             <button className='btn btn-accent col-span-1' onClick={()=>handleTeamDocuments}>Manage Team Role Documents</button>
         </div>
     );
